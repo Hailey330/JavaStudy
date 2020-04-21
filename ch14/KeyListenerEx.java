9b3fead00c96de99fe571ef4037c7905a2f8e712
baegopa <baegopa@wamwam.com>
2020-04-21 17:56:39 +0900

    이벤트 처리

---
+++ ch14/KeyListenerEx.java
@@ -0,0 +1,16 @@
+package ch14;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class KeyListenerEx extends JFrame {
+
+	private JLabel [] keyMessage; // 3개의 메시지를 출력할 레이블 컴포넌트 배열
+	
+	public KeyListenerEx() {
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
